f2c6bd5e17af89f8e538b7df72a5b2a6e618a6c6
thehoangbka1997 <thehoangbka1997@gmail.com>
2019-12-11 08:02:25 +0000

    a

---
+++ gray.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+
+im = cv2.imread("dau7.jpg")
+gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("xam.jpg", gray)
+
