121179dda77c4c4c5c9cc35a56c60d786bb69dca
thehoangbka1997 <thehoangbka1997@gmail.com>
2019-12-13 05:05:21 +0000

    a

--- khoanhblob.py
+++ khoanhblob.py
@@ -3,47 +3,41 @@
 
 import cv2
 import numpy as np
-import sys
 
-# 画像の読み込み
-img = cv2.imread('dautach1.jpg')
+def main():
+    # 入力画像の取得
+    img = cv2.imread("dautach.jpg")
+    
+    # グレースケール変換
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# グレースケール化
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 2値化
+    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-# 大津の二値化
-gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    # ラベリング処理
+    label = cv2.connectedComponentsWithStats(gray)
 
-# 白黒反転
-gray = cv2.bitwise_not(gray)
+    # ブロブ情報を項目別に抽出
+    n = label[0] - 1
+    data = np.delete(label[2], 0, 0)
+    center = np.delete(label[3], 0, 0)  
 
-# ラベリング処理(詳細版)
-label = cv2.connectedComponentsWithStats(gray)
+    # ブロブ面積最大のインデックス
+    max_index = np.argmax(data[:,4])
 
-# オブジェクト情報を項目別に抽出
-n = label[0] - 1
-data = np.delete(label[2], 0, 0)
-center = np.delete(label[3], 0, 0)
+    # 面積最大ブロブの各種情報を表示
+    print("upper left x", data[:,0][max_index])
+    print("upper left y", data[:,1][max_index])
+    print("width", data[:,2][max_index])
+    print("high", data[:,3][max_index])
+    print("menA", data[:,4][max_index])
+    print("center:",center[max_index])
+    x1 = data[:,0][max_index]
+    y1 = data[:,1][max_index]
+    x2 = x1 + data[:,2][max_index]
+    y2 = y1 + data[:,3][max_index]
+    blob = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255))
+    cv2.imwrite("khoanhblob2.jpg", blob)
 
-# ラベリング結果書き出し用に二値画像をカラー変換
-color_src = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-
-# オブジェクト情報を利用してラベリング結果を表示
-for i in range(n):
-    # 各オブジェクトの外接矩形を赤枠で表示
-    x0 = data[i][0]
-    y0 = data[i][1]
-    x1 = data[i][0] + data[i][2]
-    y1 = data[i][1] + data[i][3]
-    cv2.rectangle(color_src, (x0, y0), (x1, y1), (0, 0, 255))
-
-    # 各オブジェクトのラベル番号と面積に黄文字で表示
-    cv2.putText(color_src, "ID: " +str(i + 1), (x0, y1 + 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
-    cv2.putText(color_src, "S: " +str(data[i][4]), (x0, y1 + 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
-
-    # 各オブジェクトの重心座標をに黄文字で表示
-    cv2.putText(color_src, "X: " + str(int(center[i][0])), (x1 - 10, y1 + 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
-    cv2.putText(color_src, "Y: " + str(int(center[i][1])), (x1 - 10, y1 + 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
-
-# 画像の保存
-cv2.imwrite('khoanhblob1.jpg', color_src)
+if __name__ == '__main__':
+    main()
